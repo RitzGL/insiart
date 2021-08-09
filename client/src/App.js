3442bc4a168e65301b3e7d1372694dc820ab8dcc
Ritz GL <maurigarzonlara@hotmail.com>
2021-08-09 12:30:30 +0800

    Footer Styled

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
-  return (
+  return (<>
     <Header />
+    <Footer />
+    </>
   );
 }
 
---
+++ client/src/components/Footer/index.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./styles.css";
+
+function Footer() {
+  return (
+    <footer>
+      <ul>
+        <li>
+          <a href="https://github.com/RitzGL/">GitHub</a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/mauricio-garzon-lara-927454206/">
+            LinkedIn
+          </a>
+        </li>
+      </ul>
+    </footer>
+  );
+}
+
+export default Footer;
